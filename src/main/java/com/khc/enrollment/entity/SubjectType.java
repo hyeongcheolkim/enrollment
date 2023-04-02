f24e009e2cbe1406f1462a51647ab77d2e28bb5f
hyeongcheolkim <tori1753@naver.com>
2023-04-03 02:18:17 +0900

    feat: SubjectType에 String Korean 필드 추가

--- src/main/java/com/khc/enrollment/entity/SubjectType.java
+++ src/main/java/com/khc/enrollment/entity/SubjectType.java
@@ -1,8 +1,18 @@
 package com.khc.enrollment.entity;
 
 public enum SubjectType {
-    GENERAL_ELECTIVE,
-    GENERAL_CORE,
-    MAJOR_ELECTIVE,
-    MAJOR_CORE,
+    GENERAL_ELECTIVE("교양선택"),
+    GENERAL_CORE("교양필수"),
+    MAJOR_ELECTIVE("전공선택"),
+    MAJOR_CORE("전공필수");
+
+    private String korean;
+
+    SubjectType(String korean) {
+        this.korean = korean;
+    }
+
+    public String getKorean() {
+        return korean;
+    }
 }
