14738853b188bbcacc3dd6ff76f864904c6a4028
hyeongcheolkim <tori1753@naver.com>
2023-04-03 02:20:01 +0900

    feat: MajorType에 String Korean 필드 추가

--- src/main/java/com/khc/enrollment/entity/MajorType.java
+++ src/main/java/com/khc/enrollment/entity/MajorType.java
@@ -1,7 +1,17 @@
 package com.khc.enrollment.entity;
 
 public enum MajorType {
-    ORIGIN,
-    SUB,
-    MULTI,
+    ORIGIN("본전공"),
+    SUB("부전공"),
+    MULTI("복수전공");
+
+    private String korean;
+
+    MajorType(String korean) {
+        this.korean = korean;
+    }
+
+    public String getKorean() {
+        return korean;
+    }
 }
